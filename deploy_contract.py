6a5be77188ab42ae00c78a79a0eaa7f641b01492
zeerorg <r.g.gupta@outlook.com>
2018-02-20 19:12:02 +0530

    COmpiled contract

--- deploy_contract.py
+++ deploy_contract.py
@@ -9,62 +9,16 @@ w3.isConnected()
 def main():
     global contract_instance, w3
     import eth_utils
-
-    from solc import compile_source
     from web3.contract import ConciseContract
+    import json
 
-    contract_source_code = '''
-    pragma solidity ^0.4.18;
-    // We have to specify what version of compiler this code will compile with
-    
-    contract Voting {
-      /**
-      * A map of candidate names to integer values
-      */
-      mapping (bytes32 => uint8) public votesReceived;
-    
-      /**
-            List of candidates. bytes32 array is used since ethereum doesn't support strings
-       */
-      bytes32[] public candidateList;
-    
-      /* Constructor called when we deploy app to blockchain
-      we will pass an array of candidates who will be contesting in the election
-      */
-      function Voting(bytes32[] candidateNames) public {
-        candidateList = candidateNames;
-      }
-    
-      // This function returns the total votes a candidate has received so far
-      function totalVotesFor(bytes32 candidate) view public returns (uint8) {
-        require(validCandidate(candidate));
-        return votesReceived[keccak256(candidate)];
-      }
-    
-      // This function increments the vote count for the specified candidate. This
-      // is equivalent to casting a vote
-      function voteForCandidate(bytes32 candidate) public {
-        require(validCandidate(candidate));
-        votesReceived[keccak256(candidate)] += 1;
-      }
-    
-      // checks if a candidate is valid or not
-      function validCandidate(bytes32 candidate) view public returns (bool) {
-        for (uint i = 0; i < candidateList.length; i++) {
-          if (candidateList[i] == candidate) {
-            return true;
-          }
-        }
-        return false;
-      }
-    }
-    '''
+    with open('compiled_contract.json') as f:
+        compiled_sol = json.load(f)["contracts"]["contract.sol:Voting"]
 
-    compiled_sol = compile_source(contract_source_code)
-    contract_interface = compiled_sol['<stdin>:Voting']
+    abi = json.loads(compiled_sol["abi"])
 
     # create contract for deployment
-    contract = w3.eth.contract(abi=contract_interface['abi'], bytecode=contract_interface['bin'])
+    contract = w3.eth.contract(abi=abi, bytecode=compiled_sol['bin'])
 
     # deploy contract
     tx_hash = contract.deploy(
@@ -77,7 +31,7 @@ def main():
     contract_address = tx_receipt['contractAddress']
 
     # get main contract instance
-    contract_instance = w3.eth.contract(contract_interface['abi'], contract_address, ContractFactoryClass=ConciseContract)
+    contract_instance = w3.eth.contract(abi, contract_address, ContractFactoryClass=ConciseContract)
 
     contract_instance.voteForCandidate(eth_utils.force_obj_to_bytes("Rama"), transact={'from': w3.eth.accounts[0]})
 
