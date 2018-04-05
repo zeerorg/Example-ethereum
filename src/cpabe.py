7dd985c63e1060c86311e4110c3ca172cd722734
zeerorg <r.g.gupta@outlook.com>
2018-04-05 08:47:23 +0530

    created cpabe functions

--- src/cpabe.py
+++ src/cpabe.py
@@ -1,40 +1,39 @@
 import subprocess
 import shlex
 
-def init_module(_w3, _contract):
-    global w3, contract_instance
-    w3 = _w3
-    contract_instance = _contract
-
 '''
 functionality required : 
-1. Encrypt and Upload document to ethereum blockchain
-2. Download the encrypted document
-3. Download any requested attribute key
-4. Take a requester public key and attributes,
-     and upload their attribute key to blockchain after encrypting it with their public key
+1. generate a new key in "./key" directory and encrypt document using that key
+2. generate a new key for the access control
+3. decrypt using the given key
 '''
 #initial cpabe_setup:: pub_key and master_key is generated
-subprocess.call("./cpabe-setup")
+def gen_pair():
+    subprocess.call(["mkdir", "./key"])
+    subprocess.call("./key/cpabe-setup")
 
 #cpabe-keygen:: sara_priv_key will be generated
-command_line = input()
-args = shlex.split(command_line)
-print(args)
-#tmp=subprocess.call(['./cpabe-keygen', '-o', 'sara_priv_key', 'pub_key', 'master_key', 1, 1, 1])
-subprocess.Popen(args)
+# command_line = input()
+def gen_priv_key(name):
+    command_line = "cpabe-keygen -o ./key/{}_priv_key ./key/pub_key ./key/master_key 1 1 1".format(name)
+    args = shlex.split(command_line)
+    print(args)
+    #tmp=subprocess.call(['./cpabe-keygen', '-o', 'sara_priv_key', 'pub_key', 'master_key', 1, 1, 1])
+    subprocess.Popen(args)
 
 #cpabe-enc :: encrypted security_report.pdf.cpabe is generated
-command_line1 = input()
-args1 = shlex.split(command_line1)
-print(args1)
-#tmp=subprocess.call(['cpabe-enc' 'pub_key' 'security_report.pdf' 1 1 1])
-subprocess.Popen(args1)
+# command_line1 = input()
+def encrypt_report(file_name):
+    command_line = "cpabe-enc ./key/pub_key ./{} 1 1 1".format(file_name)
+    args1 = shlex.split(command_line1)
+    print(args1)
+    #tmp=subprocess.call(['cpabe-enc' 'pub_key' 'security_report.pdf' 1 1 1])
+    subprocess.Popen(args1)
 
-#cpabe-dec:: security_report.pdf will be generated on successful decryption 
-command_line2 = input()
-args2 = shlex.split(command_line2)
-print(args2)
-#tmp=subprocess.call(['cpabe-dec'  'pub_key' 'sara_priv_key' 'master_key' 'security_report.pdf.cpabe'])
-subprocess.Popen(args2)
-
+#cpabe-dec:: security_report.pdf will be generated on successful decryption
+def dec_file(filename, attrkey)
+    command_line2 = "cpabe-dec ./key/pub_key ./key/{}  {}".format(attrkey, filename)
+    args2 = shlex.split(command_line2)
+    print(args2)
+    #tmp=subprocess.call(['cpabe-dec'  'pub_key' 'sara_priv_key' 'security_report.pdf.cpabe'])
+    subprocess.Popen(args2)
